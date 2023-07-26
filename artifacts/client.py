Add: configurable n_clusters, rotation transforms / Fix: random representation generation wrt encoder output size

artifacts/client.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 from typing import List, Tuple
 import argparse
 import copy
+import time
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
@@ -12,6 +13,7 @@ from utils.datasets import load_partition
 from utils.models import Net, LogisticRegression, AutoEncoder, EncoderNet
 from utils.partition_data import Partition
 from utils.function import train_standard_classifier, train_regression, test_standard_classifier, test_regression
+from utils.transforms import Rotate
 import logging
 import flwr as fl
 from utils.clustering_fn import compute_low_dims_per_class
@@ -46,7 +48,10 @@ class FlowerClient(fl.client.NumPyClient):
             train_regression(self.model, self.trainloader, config=config, device=DEVICE, args=args)
 
         # compute low dimensional representation of local data
-        ld = compute_low_dims_per_class(encoder, self.trainloader, device=DEVICE)
+        start = time.time()
+        ld = compute_low_dims_per_class(encoder, self.trainloader, output_size=z_dim, device=DEVICE)
+        print("Time to compute low dims: ", time.time() - start)
+        print(ld.shape)
 
         # concatenating model weights and low dim to return to the server
         params = self.get_parameters()
@@ -78,19 +83,39 @@ if __name__ == "__main__":
     parser.add_argument(
         "--transform", type=str, required=False, default=None, help="Transform to apply to input data"
     )
+    parser.add_argument(
+        "--path_to_encoder_weights", type=str, required=False, default='/app/artifacts/enc_save_orig.pth', help="Path to encoder weights"
+    )
     args = parser.parse_args()
 
-    model = LogisticRegression(input_size=28*28, num_classes=10).to(DEVICE)
+    if args.model == "regression":
+        model = LogisticRegression(input_size=28*28, num_classes=10).to(DEVICE)
+    elif args.model == "cnn":
+        model = Net().to(DEVICE)
+    else:
+        try:
+            raise ValueError('Invalid model name')
+        except ValueError as err:
+            logging.info('Invalid model name')
+            raise
 
     # Testing
-    #encoder = AutoEncoder().to(DEVICE).encoder
+    # encoder = AutoEncoder().to(DEVICE).encoder
+    #z_dim = 100
     encoder = EncoderNet().to(DEVICE)
-    encoder.load_state_dict(torch.load('/app/artifacts/enc_save_orig.pth'))
+    encoder.load_state_dict(torch.load(args.path_to_encoder_weights, map_location=torch.device(DEVICE)))
+    z_dim = 2
 
     if args.transform == "solarize":
         transform = transforms.Compose([transforms.RandomSolarize(threshold=200.0), transforms.ToTensor()])
-    if args.transform == "elastic":
+    elif args.transform == "elastic":
         transform = transforms.Compose([transforms.ElasticTransform(alpha=100.0), transforms.ToTensor()])
+    elif args.transform == "rotate90":
+        transform = transforms.Compose([Rotate(90), transforms.ToTensor()])
+    elif args.transform == "rotate180":
+        transform = transforms.Compose([Rotate(180), transforms.ToTensor()])
+    elif args.transform == "rotate270":
+        transform = transforms.Compose([Rotate(270), transforms.ToTensor()])
     else:
         transform = transforms.Compose(
             [transforms.ToTensor()]

artifacts/strategies/TestEncoding.py
@@ -34,7 +34,7 @@ class TestEncoding(TensorboardStrategy):
         eval_fn,
         writer,
         on_fit_config_fn,
-        num_clients_to_keep=1):
+        n_clusters):
 
         super().__init__(min_fit_clients=min_fit_clients, 
                         min_available_clients=min_available_clients, 
@@ -45,7 +45,8 @@ class TestEncoding(TensorboardStrategy):
                         writer=writer)
         
         self.writer = writer
-        self.num_clients_to_keep = num_clients_to_keep
+        self.n_clusters = n_clusters
+        # self.cluster_centers = None
 
     def __repr__(self) -> str:
         return "TestEncoding"
@@ -76,14 +77,20 @@ class TestEncoding(TensorboardStrategy):
         ]
         low_dims = [np.array(sample).flatten() for sample in low_dims]
         print("low_dims size: ", np.array(low_dims).shape)
+        print(low_dims)
 
         cluster_truth = [
             fit_res.metrics for _, fit_res in results
         ]
 
         # Building clusters
-        cluster_labels, cluster_centers = make_clusters(low_dims, n_clusters=3)
-        print_clusters(cluster_labels, cluster_truth, n_clusters=3)
+        cluster_labels, cluster_centers = make_clusters(low_dims, n_clusters=self.n_clusters)
+        # if self.cluster_centers is None:
+        #     cluster_labels, self.cluster_centers = make_clusters(low_dims, n_clusters=self.n_clusters)
+        # else:
+        #     cluster_labels, self.cluster_centers = make_clusters(low_dims, n_clusters=self.n_clusters, cluster_centers=self.cluster_centers)
+
+        print_clusters(cluster_labels, cluster_truth, n_clusters=self.n_clusters)
 
         parameters_aggregated = ndarrays_to_parameters(
             aggregate(weights_results)

artifacts/utils/clustering_fn.py
@@ -3,8 +3,6 @@ import numpy as np
 from sklearn.cluster import KMeans
 import logging
 
-z_dim = 100
-
 def select_samples(dataloader, label, device):
     chosen_indices = []
     samples = []
@@ -25,27 +23,27 @@ def split_by_class(dataloader, device):
     return samples
 
 
-def compute_low_dims(net, batch):
+def compute_low_dims(net, batch, output_size):
     try:
         return net(torch.stack(batch))
     except:
-        return torch.rand(1, z_dim)
+        return torch.rand(1, output_size)
 
 
 def compute_avg(low_dim):
     return low_dim.mean(0).cpu().detach().numpy()
 
 
-def compute_low_dims_per_class(net, dataloader, device):
+def compute_low_dims_per_class(net, dataloader, output_size, device):
     ld = np.array([])
     class_batches = split_by_class(dataloader, device=device)
     for class_id in range(10):
-        ld = np.append(ld, compute_avg(compute_low_dims(net, class_batches[class_id])))
+        ld = np.append(ld, compute_avg(compute_low_dims(net, class_batches[class_id], output_size)))
     return ld
 
 
 def make_clusters(low_dims, n_clusters):
-    kmeans = KMeans(n_clusters=n_clusters).fit(low_dims)
+    kmeans = KMeans(n_clusters=n_clusters, n_init=10).fit(low_dims)
     centers = kmeans.cluster_centers_
     labels = kmeans.labels_
 

artifacts/utils/models.py
@@ -164,12 +164,12 @@ class EncoderNet(nn.Module):
                                 nn.Linear(256, 2)
                                 )
 
-        def forward(self, x):
-            output = self.convnet(x)
-            # print(f'after convnet' + str(output.size()))
-            output = output.view(output.size()[0], -1)
-            output = self.fc(output)
-            return output
+    def forward(self, x):
+        output = self.convnet(x)
+        # print(f'after convnet' + str(output.size()))
+        output = output.view(output.size()[0], -1)
+        output = self.fc(output)
+        return output
 
-        def get_encoding(self, x):
-            return self.forward(x)
+    def get_encoding(self, x):
+        return self.forward(x)